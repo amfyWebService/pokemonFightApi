7145a3b3a225eeecd53711b632033e4741bd9b70
Farsen976 <farsen.abdallahcombo@ynov.com>
2020-03-12 20:36:40 +0100

    green(gameManager): should return current state of a fight

--- src/main/kotlin/com/ynov/pokemon/model/GameManager.kt
+++ src/main/kotlin/com/ynov/pokemon/model/GameManager.kt
@@ -1,11 +1,42 @@
 package com.ynov.pokemon.model
 
-class GameManager(val player: Trainer, val ai: Trainer){
+import com.lectra.koson.arr
+import com.lectra.koson.obj
+
+class GameManager(val player: Trainer, val ai: Trainer) {
     fun gameState(): String {
-        return ""
+        return obj {
+            "player1" to player.toJson()
+            "player2" to ai.toJson()
+        }.toString()
     }
 
     private fun isAllPokemonsKo(trainer: Trainer): Boolean {
         return false
     }
+}
+
+fun Trainer.toJson() = obj {
+    "name" to name
+    "pokemons" to arr[backPack.pokemons.map { pokemon ->
+        obj {
+            "name" to pokemon.name
+            "type" to pokemon.type
+            "currentHealthPoint" to pokemon.currentHealthPoint
+            "maxHealthPoint" to pokemon.maxHealthPoint
+            "attacks" to arr[pokemon.attacks.map { attack ->
+                obj {
+                    "name" to attack.name
+                    "damage" to attack.damage
+                }
+            }]
+        }
+    }]
+    "items" to arr[
+        backPack.items.map { item ->
+            obj {
+                "name" to item.name
+                "description" to item.description
+            }
+    }]
 }
