954f53f4cceeabca4608e8fe38c6afb32598c9da
Goms <gomesdpro@gmail.com>
2020-03-12 16:59:00 +0100

    green(item): potion should not overtaken maxHealthPoint of the pokemon

--- src/main/kotlin/com/ynov/pokemon/model/Potion.kt
+++ src/main/kotlin/com/ynov/pokemon/model/Potion.kt
@@ -1,8 +1,11 @@
 package com.ynov.pokemon.model
 
+import kotlin.math.min
+
 class Potion : Item("Potion", "Raise Health of 20") {
     override fun apply(pokemon: Pokemon): Pokemon {
         pokemon.currentHealthPoint += 20
+        pokemon.currentHealthPoint = min(pokemon.currentHealthPoint, pokemon.maxHealthPoint)
         return pokemon
     }
 }
