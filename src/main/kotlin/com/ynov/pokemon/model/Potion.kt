8ab2d0029d478adbe54aeb9d2992c71e8f11871b
Goms <gomesdpro@gmail.com>
2020-03-12 16:51:10 +0100

    green(item): potion should increase health point

--- src/main/kotlin/com/ynov/pokemon/model/Potion.kt
+++ src/main/kotlin/com/ynov/pokemon/model/Potion.kt
@@ -2,6 +2,7 @@ package com.ynov.pokemon.model
 
 class Potion : Item("Potion", "Raise Health of 20") {
     override fun apply(pokemon: Pokemon): Pokemon {
+        pokemon.currentHealthPoint += 20
         return pokemon
     }
 }
