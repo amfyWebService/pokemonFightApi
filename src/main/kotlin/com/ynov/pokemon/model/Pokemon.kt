e8fde83c1aaf84ada0af9b88cb4ac1598b5d0839
mano <mano>
2020-03-12 16:25:58 +0100

    green(Pokemon):should apply damage on pokemon

--- src/main/kotlin/com/ynov/pokemon/model/Pokemon.kt
+++ src/main/kotlin/com/ynov/pokemon/model/Pokemon.kt
@@ -1,12 +1,12 @@
 package com.ynov.pokemon.model
 
-data class Pokemon(val name : String, val currentHealthPoint : Int, val maxHealthPoint : Int , val type : String, val attacks: List<Attack>) {
+data class Pokemon(val name : String, var currentHealthPoint : Int, val maxHealthPoint : Int , val type : String, val attacks: List<Attack>) {
     fun attack(pokemon : Pokemon, attackType: Attack) : Pokemon{
         return Pokemon("", 0, 0, "", emptyList())
     }
 
     fun applyDamage(damage : Int){
-
+        this.currentHealthPoint -= damage
     }
 
     fun isKo() : Boolean{
