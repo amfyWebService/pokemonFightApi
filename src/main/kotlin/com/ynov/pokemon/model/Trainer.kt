127f340adb99a78f533be623879f522b3709e228
mano <mano>
2020-03-12 17:10:38 +0100

    green(Trainer): should have a default pokemon

--- src/main/kotlin/com/ynov/pokemon/model/Trainer.kt
+++ src/main/kotlin/com/ynov/pokemon/model/Trainer.kt
@@ -1,6 +1,13 @@
 package com.ynov.pokemon.model
 
-data class Trainer(val name : String, val currentPokemon : Pokemon?, val backPack : BackPack) {
+data class Trainer(val name : String, var currentPokemon : Pokemon?, val backPack : BackPack) {
+    init {
+        if (currentPokemon == null) {
+            currentPokemon = backPack.pokemons.random()
+        }
+
+    }
+
     fun useItem(item : Item, pokemon : Pokemon): Pokemon{
         return Pokemon("", 0, 0, "", emptyList())
 
