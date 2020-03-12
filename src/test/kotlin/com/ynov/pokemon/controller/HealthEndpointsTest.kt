05df07f3772d6275c5066dae128cd18e38ce3320
Farsen976 <farsen.abdallahcombo@ynov.com>
2020-03-12 15:08:23 +0100

    green(health): first test

---
+++ src/test/kotlin/com/ynov/pokemon/controller/HealthEndpointsTest.kt
@@ -0,0 +1,28 @@
+package com.ynov.pokemon.controller
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class HealthEndpointsTest {
+    @Autowired
+    private val mvc: MockMvc? = null
+
+    @Test
+    @Throws(Exception::class)
+    fun getHello() {
+        mvc!!.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("hello world")))
+    }
+}
