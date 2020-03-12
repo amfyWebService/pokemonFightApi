610b6167fc6be22218cc0c08ade5ba89903d84ee
mano <mano>
2020-03-12 16:28:53 +0100

    green(Pokemon):should attack a pokemon

--- src/main/kotlin/com/ynov/pokemon/model/Pokemon.kt
+++ src/main/kotlin/com/ynov/pokemon/model/Pokemon.kt
@@ -2,7 +2,8 @@ package com.ynov.pokemon.model
 
 data class Pokemon(val name : String, var currentHealthPoint : Int, val maxHealthPoint : Int , val type : String, val attacks: List<Attack>) {
     fun attack(pokemon : Pokemon, attackType: Attack) : Pokemon{
-        return Pokemon("", 0, 0, "", emptyList())
+        pokemon.applyDamage(attackType.damage)
+        return pokemon
     }
 
     fun applyDamage(damage : Int){
