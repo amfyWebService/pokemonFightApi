c7f5f78332eae10f304b467bf95baed79375b575
mano <mano>
2020-03-12 17:34:41 +0100

    red(Trainer): should not select pokemon that doesn't exist in backpack

--- src/test/kotlin/com/ynov/pokemon/model/TrainerTest.kt
+++ src/test/kotlin/com/ynov/pokemon/model/TrainerTest.kt
@@ -1,5 +1,6 @@
 package com.ynov.pokemon.model
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 
@@ -14,5 +15,19 @@ internal class TrainerTest{
         val trainer = Trainer("Sacha", null, backPack )
 
         assertNotNull(trainer.currentPokemon)
+    }
+
+    @Test
+    fun `should not select pokemon that doesn't exist in backpack`(){
+        val attackEclair = Attack("éclair", 30)
+        val pikachu = Pokemon("pikachu", 100,100, "electric", listOf(attackEclair))
+        val raichu = Pokemon("raichu", 70,100, "electric", listOf(attackEclair))
+        val ronflex = Pokemon("raichu", 70,100, "electric", listOf(attackEclair))
+        val backPack = BackPack(listOf(raichu,pikachu), emptyList())
+        val trainer = Trainer("Sacha", null, backPack )
+
+        Assertions.assertThrows(IllegalStateException::class.java){
+            trainer.pickUpPokemon(ronflex)
+        }
     }
 }
