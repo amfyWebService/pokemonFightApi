cc0be999d4098c9d4add7574c8a4f2b77ad490e3
mano <mano>
2020-03-12 17:48:55 +0100

    green(Trainer): should select pokemon that exist in backpack

--- src/main/kotlin/com/ynov/pokemon/model/Trainer.kt
+++ src/main/kotlin/com/ynov/pokemon/model/Trainer.kt
@@ -17,5 +17,6 @@ data class Trainer(val name : String, var currentPokemon : Pokemon?, val backPac
         if(!this.backPack.pokemons.contains(pokemon)){
             throw IllegalStateException("You can't select a pokemon that doesn't exist in your backpack")
         }
+        this.currentPokemon = pokemon
     }
 }
