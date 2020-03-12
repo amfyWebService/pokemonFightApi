736b21898cec17dfed5aed177d959ccc43c718cc
mano <mano>
2020-03-12 17:48:19 +0100

    red(Trainer): should select pokemon that exist in backpack

--- src/test/kotlin/com/ynov/pokemon/model/TrainerTest.kt
+++ src/test/kotlin/com/ynov/pokemon/model/TrainerTest.kt
@@ -3,6 +3,7 @@ package com.ynov.pokemon.model
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class TrainerTest{
 
@@ -29,5 +30,17 @@ internal class TrainerTest{
         Assertions.assertThrows(IllegalStateException::class.java){
             trainer.pickUpPokemon(ronflex)
         }
+    }
+
+    @Test
+    fun `should select pokemon that exist in backpack`(){
+        val attackEclair = Attack("éclair", 30)
+        val pikachu = Pokemon("pikachu", 100,100, "electric", listOf(attackEclair))
+        val raichu = Pokemon("raichu", 70,100, "electric", listOf(attackEclair))
+        val backPack = BackPack(listOf(raichu,pikachu), emptyList())
+        val trainer = Trainer("Sacha", pikachu, backPack )
+        trainer.pickUpPokemon(raichu)
+
+        assertEquals(raichu, trainer.currentPokemon)
     }
 }
